3b9168c2d8536bdd0af26db04f26b1113f62aa0f
Nolan Mortenson <NSMortenson@student.fullsail.edu>
2021-01-28 13:27:18 -0500

    added stamina to the player and currently working on health and weapon mechanics

---
+++ GGJ21/Assets/Scripts/Player.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5F;
+    public float gravity = 20F;
+    //stats
+    public float health = 50F;
+    public float stamina = 5F;
+    public GameObject weapon;
+    private float refillStam;
+    private bool tired = false;
+    public float timer;
+    private float resetTimer;
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        resetTimer = timer;
+        refillStam = stamina;
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (controller.isGrounded)
+        {
+            //float delta = Input.GetAxis("Mouse ScrollWheel");
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+
+            //Player is able to run in short burst - drains stamina
+            if (Input.GetButton("Run") && stamina > 0 && tired == false)
+            {
+                speed = 10F;
+                stamina -= Time.deltaTime;
+            }
+            else
+            {
+                speed = 5F;
+                tired = true;
+            }
+
+            if (stamina < refillStam && tired == true)
+            {
+                stamina += Time.deltaTime;
+            }
+            else
+            {
+                tired = false;
+            }
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+
+    }
+}
---
+++ GGJ21/Assets/Scripts/PlayerWeapon.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWeapon : MonoBehaviour
+{
+    public float damage;
+    public float range;
+    private float nr;
+   // public float fireRate;
+   // public float knockback;
+    private GameObject enemy;
+    [NonSerialized] public PlayerWeapon currentWeapon;
+    private bool hit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentWeapon = this.GetComponent<PlayerWeapon>();
+        hit = this.GetComponent<BoxCollider>().enabled;
+        hit = false;
+
+       nr = this.GetComponent<BoxCollider>().size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //set the stats to the current weapon
+        damage = currentWeapon.GetComponent<PlayerWeapon>().damage;
+        range = currentWeapon.GetComponent<PlayerWeapon>().range;
+        nr = range;
+        //if the player presses the left mouse click 
+        //swing the weapon
+        if (Input.GetButton("Fire1"))
+        {
+            //play animation and activate damage
+            hit = true;
+        }
+        else
+        {
+            hit = false;
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "enemy")
+        {
+            enemy = collision.gameObject;
+        }
+        //deal damage to the enemy
+
+    }
+}
