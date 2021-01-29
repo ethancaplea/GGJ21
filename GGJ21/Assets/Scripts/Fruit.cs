372c2c8e44a76b0b8818c7c2544b940cd83e760d
Sheng Lian <SLian1@student.fullsail.edu>
2021-01-29 16:47:24 -0500

    Trap and fruit Mainmenu and settingmenu

---
+++ GGJ21/Assets/Scripts/Fruit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.GetComponent<BasicEnemyBehavior>().fruitFlag = true;
+            Debug.Log("Fruit active");
+        }
+        else if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player Fruit active");
+        }
+    }
+}
