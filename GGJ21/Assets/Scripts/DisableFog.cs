19dfc50042df8a045ec5bbc7c1b4ce773e8edc4c
EthanCaplea <emcaplea@gmail.com>
2021-01-30 03:36:06 -0500

    -Started working on tower and parts

---
+++ GGJ21/Assets/Scripts/DisableFog.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableFog : MonoBehaviour
+{
+    private void OnPreRender()
+    {
+        RenderSettings.fog = false;
+    }
+}
