5e3a45a26c0541d2dce693d9a7d1027ca33b727e
itsthenap <itsthenap@gmail.com>
2021-01-31 14:13:13 -0500

    i dont really know

---
+++ GGJ21/Assets/Scripts/SceneTransfer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransfer : MonoBehaviour
+{
+    private void OnTriggerEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("Scenes/Shed");
+        }
+
+    }
+
+
+}
