e12983271eb87ae4d952dc1d7e3900aec22c3549
itsthenap <itsthenap@gmail.com>
2021-02-01 06:08:33 -0500

    scenetransfer

---
+++ GGJ21/Assets/Scripts/SceneTransfer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransfer : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("Scenes/Shed");
+            Debug.Log("Poopy");
+        }
+
+    }
+
+
+}
